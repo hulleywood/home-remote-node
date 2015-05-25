dc35248753fb8de05c4f7c6eae0b17cf3206041c
James <james.hulley@gmail.com>
2015-05-24 22:04:05 -0700

    test buttons

---
+++ public/javascripts/main.js
@@ -0,0 +1,14 @@
+$(function(){
+  $('.remote-action').click(function(e){
+    var $btn = $(e.currentTarget),
+        device = $btn.data('device'),
+        command = $btn.data('command');
+
+    if (device && command) {
+      $("#div1").html('');
+      $.ajax({url: "send/" + device +"/" + command, success: function(result){
+        $("#div1").html(result);
+      }});
+    }
+  });
+});
